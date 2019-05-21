3b9890ed72bfe261507b4f33be976e3fe7f89843
Ilhamsat <ilhamsatriadi10@gmail.com>
2019-05-21 15:34:27 +0700

    Updates

---
+++ precache-manifest.302658ea7d98ee574f7cb5e3047c7e50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0564ed3371df53407e590a4dd36dd57",
+    "url": "/WebBiodata/index.html"
+  },
+  {
+    "revision": "75bc539bc664227f7e27",
+    "url": "/WebBiodata/static/css/2.1ad4406c.chunk.css"
+  },
+  {
+    "revision": "6afb0fae3450ea911293",
+    "url": "/WebBiodata/static/css/main.42b6ad53.chunk.css"
+  },
+  {
+    "revision": "75bc539bc664227f7e27",
+    "url": "/WebBiodata/static/js/2.bb9090e2.chunk.js"
+  },
+  {
+    "revision": "6afb0fae3450ea911293",
+    "url": "/WebBiodata/static/js/main.4cb0beec.chunk.js"
+  },
+  {
+    "revision": "d613c9a49c1a91512f47",
+    "url": "/WebBiodata/static/js/runtime~main.e59f3506.js"
+  },
+  {
+    "revision": "ce42b93fe9a258f6c9dc15323cb78d2a",
+    "url": "/WebBiodata/static/media/bio.ce42b93f.jpg"
+  },
+  {
+    "revision": "2593cbb4f7444985c3460c4849ea3c0a",
+    "url": "/WebBiodata/static/media/iceberg.2593cbb4.png"
+  },
+  {
+    "revision": "e8201068a4ee488ff5d59b219dac196f",
+    "url": "/WebBiodata/static/media/tempodoeloe.e8201068.jpg"
+  }
+]);
